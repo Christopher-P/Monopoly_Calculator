ef70170c1374aaac56fbe7dd609766e76986023a
ChristopherP <fake@example.com>
2013-12-27 12:42:15 -0800

    First commit, still starting code

---
+++ Monopoly_Calculator.cpp
@@ -0,0 +1,29 @@
+// Monopoly_Calculator.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+void greetings();
+void Functions();
+
+int main()
+{
+	greeting();
+	Functions();
+	return 0;
+}
+
+void greeting()
+{
+	cout << "Hi";
+	cout << "This Program Will Calculate monopoly movement";
+	system("Pause");
+}
+
+void Functions()
+{
+
+}
